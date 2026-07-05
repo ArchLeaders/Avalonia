0103039903385c658f6ac0e520f397bdaf8065ea
ArchLeaders <archleadership28@gmail.com>
2026-07-05 11:35:04 -0700

    fix(controls): Skip selection update on press when the Control key modifier is present
    Fixes selection being nullified when using Control+Click to select multiple items

--- src/Avalonia.Controls/Primitives/ItemSelectionEventTriggers.cs
+++ src/Avalonia.Controls/Primitives/ItemSelectionEventTriggers.cs
@@ -33,7 +33,7 @@ public static class ItemSelectionEventTriggers
             // Select on mouse press, unless the mouse can generate gestures
             { Pointer.Type: PointerType.Mouse } => InputElement.GetIsHoldWithMouseEnabled(selectable)
                 ? eventArgs.RoutedEvent == InputElement.PointerReleasedEvent
-                : eventArgs.RoutedEvent == InputElement.PointerReleasedEvent || selectable is ISelectable { IsSelected: false },
+                : eventArgs.RoutedEvent == InputElement.PointerReleasedEvent || (selectable is ISelectable { IsSelected: false } && !eventArgs.KeyModifiers.HasAllFlags(KeyModifiers.Control)),
 
             // Pen "right clicks" are used for context menus, and gestures are only processed for primary input
             { Pointer.Type: PointerType.Pen, Properties.PointerUpdateKind: PointerUpdateKind.RightButtonPressed or PointerUpdateKind.RightButtonReleased } =>
